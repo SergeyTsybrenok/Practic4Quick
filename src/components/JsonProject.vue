b3aa435e470af8e0cf53645144f38b810b1292af
TheRose20 <cybrenok91@mail.ru>
2026-03-26 15:02:15 +0300

    add fueture download json

--- src/components/JsonProject.vue
+++ src/components/JsonProject.vue
@@ -5,9 +5,11 @@
     <h2 class="text-center"></h2>
     
     <v-col cols="12" md="5">
-      <h2>Json editor</h2>
+      <h2>Json data</h2>
     </v-col>
-    <v-file-upload title="Drag and drop Json Here"></v-file-upload>
+    <!-- TODO load data from json -->
+    <!-- <v-file-upload htitle="Drag and drop Json Here"></v-file-upload> -->
+     <button @click="ExampleDownload()">Download current data</button>
   </v-row>
 </v-container>
 
@@ -15,6 +17,21 @@
 </template>
 <script setup lang="ts">
 import { usePopup } from '@/composables/usePopup';
+import { useAppStore } from '@/stores/app';
+import { useProductStore } from '@/stores/useProductStore';
+import { downloadObjectAsJSONFile } from '@/tools/downloadObjectAsJson';
+
+const users = useAppStore();
+const products = useProductStore();
+
+function ExampleDownload() {
+  const saveData: object = {
+    users: users,
+    products: products,
+  }
+  downloadObjectAsJSONFile(saveData, new Date().toLocaleTimeString());
+  popup.showMessage("Data was downloaded", "success");
+}
 
 const popup = usePopup();
 
