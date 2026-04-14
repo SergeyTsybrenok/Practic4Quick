c84390852d0d80918fb9875a338802ca61b1d5ce
TheRose20 <cybrenok91@mail.ru>
2026-04-14 11:49:34 +0300

    Filters

---
+++ src/components/sub-components/MultipleSelect.vue
@@ -0,0 +1,30 @@
+<template>
+      <v-select
+        :min-width="width"
+        :max-width="width"
+        :v-model="model"
+        :items="items"
+        :label="label"
+        multiple
+      >
+        <template v-slot:selection="{ item, index }">
+          <v-chip v-if="index < 2" :text="item"></v-chip>
+
+          <span
+            v-if="index === 2"
+            class="text-grey text-body-small align-self-center"
+          >
+            (+{{ model.length - 2 }} others)
+          </span>
+        </template>
+      </v-select>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  model: any,
+  items: any[],
+  width: number,
+  label: string
+}>();
+</script>
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,17 +1,79 @@
 <template>
   <v-container>
     <h1>Catalog</h1>
-    <v-card class="my-4 d-flex ga-3">
+    <v-card class="d-flex flex-wrap my-4 ga-3">
+      <div>
+        <v-icon size="x-large">mdi-filter</v-icon>
+        Filters
+        <v-btn color="info" text="Clear" @click="resetFilters()"></v-btn>
+      </div>
       <v-text-field
+        min-width="200"
         name="Search"
         label="Search"
         v-model="searchText"
         required
       ></v-text-field>
       <v-select
+        width="300"
+        min-width="200"
         :items="keyboardType.Switch"
-        v-model="searchType"
-        label="Type"
+        v-model="searchTypes.switch"
+        label="Switch"
+        multiple
+        chips
+      ></v-select>
+      <v-select
+        width="300"
+        min-width="200"
+        :items="keyboardType.Color"
+        v-model="searchTypes.color"
+        label="Color"
+        multiple
+        chips
+      ></v-select>
+      <v-select
+        width="100"
+        min-width="200"
+        :items="keyboardType.Keycaps"
+        v-model="searchTypes.keycaps"
+        label="Keycaps"
+        multiple
+        chips
+      ></v-select>
+      <v-select
+        width="100"
+        min-width="200"
+        :items="keyboardType.Size"
+        v-model="searchTypes.size"
+        label="Size"
+        multiple
+        chips
+      ></v-select>
+      <v-select
+        width="100"
+        min-width="200"
+        :items="keyboardType.Wire"
+        v-model="searchTypes.wireless"
+        label="Connect type"
+        multiple
+        chips
+      ></v-select>
+      <v-select
+        width="100"
+        min-width="200"
+        :items="keyboardType.Hotswap"
+        v-model="searchTypes.hotswap"
+        label="Hotswap"
+        multiple
+        chips
+      ></v-select>
+      <v-select
+        width="100"
+        min-width="200"
+        :items="keyboardType.Solid"
+        v-model="searchTypes.split"
+        label="Form"
         multiple
         chips
       ></v-select>
@@ -27,7 +89,8 @@
           </template>
 
           <v-card-text>
-            There are no products available, please change the filters to see the producst.
+            There are no products available, please change the filters to see
+            the producst.
           </v-card-text>
 
           <v-card-actions>
@@ -46,31 +109,104 @@
 
 <script setup lang="ts">
 import ProductCard from "@/components/ProductCard.vue";
+import MultipleSelect from "@/components/sub-components/MultipleSelect.vue";
 import { usePopup } from "@/composables/usePopup";
 import { useProductStore } from "@/stores/useProductStore";
 import keyboardType from "@/types/keyboardType";
+import type { Product } from "@/types/product";
 import { computed, reactive, ref } from "vue";
 
 const { products, addProduct, removeProductByIndex } = useProductStore();
 
 const popup = usePopup();
 
-const searchType = ref();
+const searchTypes = reactive({
+  switch: [],
+  color: [],
+  size: [],
+  keycaps: [],
+  wireless: [],
+  hotswap: [],
+  split: [],
+});
 const searchText = ref<string>();
-const searchedProducts = computed(() => {
-  if (!searchText.value?.trim()) {
-    return products;
+
+const matchesFilters = (product: Product): boolean => {
+  if (
+    searchText.value?.trim() &&
+    !product.name?.toLowerCase().includes(searchText.value.toLowerCase()) &&
+    !product.description?.toLowerCase().includes(searchText.value.toLowerCase())
+  ) {
+    return false;
   }
 
-  const searchLower = searchText.value.toLocaleLowerCase();
-  return products.filter(
-    (products) => products.name.toLocaleLowerCase().includes(searchLower),
-    // || products.type.toLocaleLowerCase().includes(searchLower),
-  );
-});
+  const checkArray = (
+    field: string | undefined,
+    filters: string[],
+  ): boolean => {
+    if (filters.length === 0) return true;
+    if (!field) return false;
+    return filters.some((f) => field.toLowerCase().includes(f.toLowerCase()));
+  };
 
-function resetFilters () {
+  const checkArrayNumber = (
+    field: number | undefined,
+    filters: number[],
+  ): boolean => {
+    if (filters.length === 0) return true;
+    if (!field) return false;
+    return filters.some((f) => field === f);
+  };
+
+  const checkBoolean = (
+    field: boolean | undefined,
+    filters: boolean[],
+  ): boolean => {
+    if (filters.length === 0) return true;
+    return filters.includes(!!field);
+  };
+
+  const getStringUseBoolean = (value: boolean, list: string[]): string => {
+    if (value) {
+      return list[0];
+    } else {
+      return list[1];
+    }
+  };
+
+  return (
+    checkArray(product.switch, searchTypes.switch) &&
+    checkArray(product.color, searchTypes.color) &&
+    checkArrayNumber(product.size, searchTypes.size) &&
+    checkArray(product.keycaps, searchTypes.keycaps) &&
+    checkArray(
+      getStringUseBoolean(!product.wireless, keyboardType.Wire),
+      searchTypes.wireless,
+    ) &&
+    checkArray(
+      getStringUseBoolean(product.hotswap, keyboardType.Hotswap),
+      searchTypes.hotswap,
+    ) &&
+    checkArray(
+      getStringUseBoolean(!product.split, keyboardType.Solid),
+      searchTypes.split,
+    )
+  );
+};
+
+const searchedProducts = computed(() => products.filter(matchesFilters));
+
+function resetFilters() {
   searchText.value = "";
-  popup.showMessage("Filetrs reseted", "info")
+
+  searchTypes.switch = [];
+  searchTypes.color = [];
+  searchTypes.size = [];
+  searchTypes.keycaps = [];
+  searchTypes.wireless = [];
+  searchTypes.hotswap = [];
+  searchTypes.split = [];
+
+  popup.showMessage("Filter cleared", "info");
 }
 </script>
